8658f25abd4015bcd9fd0c6a1457204c1483b9ff
Arefin Ahammed <arefinahammed63@gmail.com>
2023-04-13 06:48:17 +0100

    Both automated email works. working on getting values

--- server/automatedEmails.php
+++ server/automatedEmails.php
@@ -14,31 +14,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($functionName === 'sendQueryEmail') {
         sendQueryEmail($dataBody);
     }
+    else {
+        sendAppointmentEmail($dataBody);
+    }
 }
 
-function sendQueryEmail($data) {
-    $to = $data["to"];
-    $subject = $data["subject"];
-    $message = $data["description"];
+
+function sendQueryEmail($data): void
+{
     $mail = new PHPMailer(true);
     try {
         //Server settings
-        $mail->isSMTP();                                                                    // Send using SMTP
-        $mail->Host = 'smtp.office365.com';                                                 // SMTP server
-        $mail->SMTPAuth = true;                                                             // Enable SMTP authentication
-        $mail->Username = 'kishoreganjsender@outlook.com';                                    // SMTP username
-        $mail->Password = 'Arfi20 Narutoriri Enterokinase Vondo ChickenLegPiece@';                                                       // SMTP password
-        $mail->SMTPSecure = 'tls';                                                          // Enable TLS encryption
-        $mail->Port = 587;                                                                  // TCP port to connect to
+        $mail->isSMTP();                                                                  // Send using SMTP
+        $mail->Host = 'smtp.office365.com';                                               // SMTP server
+        $mail->SMTPAuth = true;                                                           // Enable SMTP authentication
+        $mail->Username = 'kishoreganjsender@outlook.com';                                // SMTP username
+        $mail->Password = 'Arfi20 Narutoriri Enterokinase Vondo ChickenLegPiece@';        // SMTP password
+        $mail->SMTPSecure = 'tls';                                                        // Enable TLS encryption
+        $mail->Port = 587;                                                                // TCP port to connect to
 
         //Recipients
-        $mail->setFrom('kishoreganjsender@outlook.com', 'KDC Ahammed');
-        $mail->addAddress($to, 'Testiinnnggg');         // Add a recipient
+        $mail->setFrom('kishoreganjsender@outlook.com', 'RDC WbstSender');
+        $mail->addAddress("kdcqueries@outlook.com");                              // Add a recipient
 
         // Content
-        $mail->isHTML(false);                                        // Set email format to HTML
-        $mail->Subject = $subject;
-        $mail->Body = $message;
+        $mail->isHTML(false);
+        $mail->Subject = $data["subject"];
+        $mail->Body = $data["description"];
 
         $mail->send();
         echo json_encode("Email sent successfully!");
@@ -47,3 +49,32 @@ function sendQueryEmail($data) {
     }
 }
 
+
+function sendAppointmentEmail($data): void
+{
+    $mail = new PHPMailer(true);
+    try {
+        //Server settings
+        $mail->isSMTP();                                                                  // Send using SMTP
+        $mail->Host = 'smtp.office365.com';                                               // SMTP server
+        $mail->SMTPAuth = true;                                                           // Enable SMTP authentication
+        $mail->Username = 'kishoreganjsender@outlook.com';                                // SMTP username
+        $mail->Password = 'Arfi20 Narutoriri Enterokinase Vondo ChickenLegPiece@';        // SMTP password
+        $mail->SMTPSecure = 'tls';                                                        // Enable TLS encryption
+        $mail->Port = 587;                                                                // TCP port to connect to
+
+        //Recipients
+        $mail->setFrom("kishoreganjsender@outlook.com", 'RDC WbstSender');
+        $mail->addAddress("kdcappointments@outlook.com");                         // Add a recipient
+
+        // Content
+        $mail->isHTML(false);
+        $mail->Subject = $data["subject"];
+        $mail->Body = $data["description"];
+
+        $mail->send();
+        echo json_encode("Email sent successfully!");
+    } catch (Exception $e) {
+        echo json_encode('Caught exception: ' . $e->getMessage() . "\n");
+    }
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,30 @@
 const BASE = "http://localhost/"
 
-async function sendQueryEmail() {
-    let data = {
-        to: 'arefin.ahammed1@gmail.com',
-        subject: 'About testing a feature of the site',
-        description: 'Hello, Hope you are doing well. I am checking the system that will be associated for taking appointments by the people. Will be back to your soon. Regards, Arefin'
+async function sendQueryEmail(event) {
+    event.preventDefault();
+    const button= event.target;
+
+    const form = button.closest('form');
+
+    const firstNameElement = form.querySelector('input[placeholder*="First Name"]');
+    const lastNameElement = form.querySelector('input[placeholder*="Last Name"]');
+    const ageElement = form.querySelector('input[placeholder*="Age"]');
+    const emailAddressElement = form.querySelector('input[placeholder*="Email Address"]');
+    const phoneNumberElement = form.querySelector('input[placeholder*="Number"]');
+    const subjectElement = form.querySelector('input[placeholder*="Subject"]');
+    const descriptionElement = form.querySelector('input[placeholder*="Description"]');
+
+
+    const formBody=
+        "Name: " + firstName + " " + lastName + "\n" +
+        "Age: " + age + "\n" +
+        "Email Address: " + emailAddress + "\n" +
+        "Phone Number: " + phoneNumber + "\n" +
+        "Description: " + description
+
+    let emailData = {
+        subject: subject,
+        description: formBody
     };
 
     const response= await fetch(BASE + 'automatedEmails.php', {
@@ -12,10 +32,73 @@ async function sendQueryEmail() {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({function: 'sendQueryEmail', data: data})
+        body: JSON.stringify({function: 'sendQueryEmail', data: emailData})
     })
     if (response.ok) {
         const data = await response.json();
         console.log(data);
     }
+}
+
+async function sendAppointmentEmail(event) {
+    event.preventDefault();
+    const button= event.target;
+
+    const form = button.closest('form');
+
+    const firstName = form.querySelector('input[placeholder*="First Name"]');
+    const lastName = form.querySelector('input[placeholder*="Last Name"]');
+    const age = form.querySelector('input[placeholder*="Age"]');
+    const emailAddress = form.querySelector('input[placeholder*="Email Address"]');
+    const phoneNumber = form.querySelector('input[placeholder*="Number"]');
+    const subject = form.querySelector('input[placeholder*="Subject"]');
+    const description = form.querySelector('input[placeholder*="Description"]');
+
+    const formBody=
+        "Name: " + firstName + " " + lastName + "\n" +
+        "Age: " + age + "\n" +
+        "Email Address: " + emailAddress + "\n" +
+        "Phone Number: " + phoneNumber + "\n" +
+        "Description: " + description
+
+    let emailData = {
+        subject: subject,
+        description: formBody
+    };
+
+    const response= await fetch(BASE + 'automatedEmails.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({function: 'sendAppointmentEmail', data: emailData})
+    })
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+    }
+}
+
+function isValidEmail(user_mail) {
+    const email = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return email.test(user_mail);
+}
+
+function isValidNumber(phone_number) {
+    const number = /^[+]880[0-9]{10}/;
+    return number.test(phone_number);
+}
+
+function setInputError(inputElement, errorMessage) {
+    const inputElementGroup = inputElement.closest('.form__input-group');
+    const errorElement = inputElementGroup.querySelector('.form__input-error-message');
+    inputElementGroup.classList.add('form__input-group--error');
+    errorElement.innerText = errorMessage;
+}
+
+function clearInputError(inputElement) {
+    const inputElementGroup = inputElement.closest('.form__input-group');
+    const errorElement = inputElementGroup.querySelector('.form__input-error-message');
+    inputElementGroup.classList.remove('form__input-group--error');
+    errorElement.innerText = '';
 }
