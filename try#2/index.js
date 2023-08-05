8aff87c197beb205242bdd10cf7bd0acd924aacf
HanzilaH <120693000+HanzilaH@users.noreply.github.com>
2023-08-05 09:02:57 +0100

    few animations added to the main page

--- try#2/index.js
+++ try#2/index.js
@@ -1,5 +1,29 @@
-document.getElementById('read-more-link').addEventListener('click', function(event) {
-    event.preventDefault();
-    var expandableText = document.getElementById('expandable-text');
-    expandableText.classList.toggle('text-truncate');
-});
+function reveal() {
+  let reveals = document.querySelectorAll('.reveal');
+  let windowHeight = window.innerHeight;
+  let revealPoint = 150;
+
+  reveals.forEach(element => {
+      let revealTop = element.getBoundingClientRect().top;
+
+      if (revealTop < windowHeight - revealPoint) {
+          if (element.classList.contains('animate-fade-in')) {
+              element.classList.add('fade-in');
+          } else if (element.classList.contains('animate-slide-in-left')) {
+              element.classList.add('slide-in-left');
+          } else if (element.classList.contains('animate-slide-in-right')) {
+            element.classList.add('slide-in-right');
+        }
+
+          element.classList.add('active');
+      } else {
+          element.classList.remove('active');
+      }
+  });
+}
+
+// Initial reveal when the page loads
+reveal();
+
+// Listen for scroll events and trigger reveal function
+window.addEventListener('scroll', reveal);
