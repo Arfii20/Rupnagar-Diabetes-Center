c1c00b54d02da35cff3785427158c9f9268cd883
Arefin Ahammed <arefin.ahammed@student.manchester.ac.uk>
2023-08-07 19:09:10 +0600

    change map and youtube. transferred images

--- server/try#2/gallery.js
+++ server/try#2/gallery.js
@@ -1,5 +1,5 @@
 const imageHtml = `<div class=" p-3 col-sm-8 col-md-6 col-lg-4" >
-                    <img src="homephoto.jpeg" class="card-img-top" alt="Image Alt Text">
+                    <img src="images/homephoto.jpeg" class="card-img-top" alt="Image Alt Text">
                 </div>`
 
 const root = document.getElementById('root')
