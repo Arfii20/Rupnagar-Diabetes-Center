4633c10308909549c8d73f389de56057274eab69
Arefin Ahammed <arefin.ahammed@student.manchester.ac.uk>
2023-08-08 00:07:06 +0600

    appointment modal done

--- server/try#2/modal.js
+++ server/try#2/modal.js
@@ -1,12 +1,8 @@
 const element = document.getElementById('entry-text');
 
 element.addEventListener("input", function (e) {
-
-    
     // this line may seem useless but its def needed
     element.style.minHeight = "30px";
-
-
     element.style.height = "auto";
     element.style.height = `${element.scrollHeight}px`;
 });
@@ -19,8 +15,7 @@ element.addEventListener('focus', (e)=>{
 
 
 
-
-document.getElementById('enter-button').addEventListener('click', function(e) {
+document.getElementById('appointment-submit-button').addEventListener('click', function(e) {
 
     // try removing this line, that will automatically check the fields however that will also reload the same page again
     e.preventDefault()
@@ -28,13 +23,10 @@ document.getElementById('enter-button').addEventListener('click', function(e) {
     console.log('button is clicked')
     console.log(document.getElementById('name-input').value)
     console.log(document.getElementById('email-input').value)
-
     console.log(document.getElementById('phone-input').value)
     console.log(document.getElementById('entry-text').value)
 
-
-
-    
     // Redirect to the home page (index.html)
     window.location.href = 'index.html'; // Replace 'index.html' with your actual homepage URL
 });
+
