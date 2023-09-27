bd85eafe1988d44395f0b7bf36916234bf3b2f21
Arefin Ahammed <arefinahammed63@gmail.com>
2023-09-27 20:45:23 +0100

    remove template, change image names and add icon

--- server/try2/gallery.js
+++ server/try2/gallery.js
@@ -1,8 +1,24 @@
-const imageHtml = `<div class=" p-3 col-sm-8 col-md-6 col-lg-4" >
-                    <img src="images/homephoto.jpeg" class="card-img-top" alt="Image Alt Text">
-                </div>`
+// const imageHtml = `<div class=" p-3 col-sm-8 col-md-6 col-lg-4" >
+//                     <img src="images/homephoto.jpeg" class="card-img-top" alt="Image Alt Text">
+//                 </div>`
+//
+// const root = document.getElementById('root')
+// for(var i = 0; i< 10; i++ ){
+//     root.insertAdjacentHTML('beforeend', imageHtml)
+// }
 
-const root = document.getElementById('root')
-for(var i = 0; i< 10; i++ ){
-    root.insertAdjacentHTML('beforeend', imageHtml)
+const galleryImage = document.getElementById("gallery_displayID");
+
+function getPictures() {
+    console.log("Hello")
+    for (let i = 0; i < 61; i++) {
+        galleryImage.innerHTML += `        
+                                    <div class="bg">
+                                        <img src="images/gallery/${i}.JPG" alt="">
+                                        <div class="bg_overlay"></div>
+                                    </div>
+                                 `
+    }
 }
+
+getPictures();
